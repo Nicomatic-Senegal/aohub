ac0babbf2bc45022a24970148b801ead5226d8b1
Kuni77 <amadoudiao72@gmail.com>
2024-02-18 23:57:18 +0500

    connexion v1

--- src/app/features/features-routing.module.ts
+++ src/app/features/features-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { MotDePasseOublieProcessusComponent } from './mot-de-passe-oublie-processus/mot-de-passe-oublie-processus.component';
 
 const routes: Routes = [
-  {path: 'signup', component: InscriptionComponent}
+  {path: 'signup', component: InscriptionComponent},
+  {path: 'signin', component: ConnexionComponent},
+  {path: 'forget-password', component: MotDePasseOublieProcessusComponent}
 ];
 
 @NgModule({
