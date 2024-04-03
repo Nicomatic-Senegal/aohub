597c065a3c3475cf8846815a0281f4655ccc5ffb
lamine gueye <laminegueye@finetechgroupe.com>
2024-04-03 13:58:29 +0000

    test

--- src/app/core/services/base-app/base-app.service.ts
+++ src/app/core/services/base-app/base-app.service.ts
@@ -13,8 +13,8 @@ export class BaseAppService {
   //baseUrlUploadDemo: string;
 
   constructor(private router: Router) {
-    this.baseUrl = 'http://inhub-plateform-backend-service:8080/api/v1/';
-    this.baseUrlAdmin = 'http://inhub-plateform-backend-service:8080/api/admin/v1/';
+    this.baseUrl = 'http://dev.plateforme-inhub.com:8080/api/v1/';
+    this.baseUrlAdmin = 'http://dev.plateforme-inhub.com:8080/api/admin/v1/';
   }
 
   getBaseUrl() {
