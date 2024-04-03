01e56b34a5821e292b5fa09098d1d202cb517ba4
Kuni77 <amadoudiao72@gmail.com>
2024-04-03 21:25:30 +0000

    save-7

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-top-bar',
@@ -7,4 +8,12 @@ import { Component, Input } from '@angular/core';
 })
 export class TopBarComponent {
   @Input() title!: string;
+
+  constructor(private authService: AuthService) {
+
+  }
+
+  logout() {
+    this.authService.logOut();
+  }
 }
