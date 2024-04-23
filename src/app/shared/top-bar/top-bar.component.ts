28f83b2c684034f4600078ec8a61886cd6f8bd36
Cheikh Oumar Ba <cheikhououmarba@finetechgroupe.com>
2024-04-23 16:11:58 +0000

    Feat: Add popup component in project

--- src/app/shared/top-bar/top-bar.component.ts
+++ src/app/shared/top-bar/top-bar.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/core/services/auth/auth.service';
+import { PopupComponent } from 'src/app/features/popup/popup.component';
 
 @Component({
   selector: 'app-top-bar',
@@ -17,7 +19,10 @@ export class TopBarComponent {
     this.languageEvent.emit(value);
   }
 
-  constructor(private translate: TranslateService, private authService: AuthService) {
+  constructor(
+    private translate: TranslateService, 
+    private authService: AuthService,
+    public dialog: MatDialog) {
     authService.isLogged()!;
     const language = localStorage.getItem("language");
     if (language) {
@@ -35,6 +40,14 @@ export class TopBarComponent {
   }
 
   logout() {
-    this.authService.logOut();
+    const title = "Êtes-vous sûr de vouloir vous deconnecter ?";
+    const description = "";
+    this.dialog.open(PopupComponent, {
+      hasBackdrop: true,
+      data: {
+        title, description
+      },
+      panelClass: 'custom-dialog-container'
+    });
   }
 }
