95f619cd1bdb3a8ec50b57e0a8ffa34e8461ac9d
ElCof <cheikhomar.cof@gmail.com>
2024-06-27 16:06:44 +0000

    Refactor: Improve paginator component to handle large number of pages with ellipses

--- src/app/features/utils/paginator/paginator.component.ts
+++ src/app/features/utils/paginator/paginator.component.ts
@@ -1,38 +1,64 @@
-import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
   styleUrls: ['./paginator.component.scss']
 })
-export class PaginatorComponent implements OnInit {
+export class PaginatorComponent implements OnInit, OnChanges {
   @Input() totalItems!: number;
-  @Input() currentPage!: number; 
-  @Input() itemPerPage!: number; 
+  @Input() currentPage!: number;
+  @Input() itemPerPage!: number;
   @Output() onClick: EventEmitter<number> = new EventEmitter();
   totalPages = 0;
   pages: number[] = [];
+  displayedPages: Array<number | string> = [];
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
-    if (this.totalItems) {
-      this.totalPages = Math.ceil(this.totalItems / this.itemPerPage);
-      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
-    }
-  }
-
-  pageClicked(page: number) {
-    if (page > this.totalPages) return;
-    this.onClick.emit(page);
+    this.calculatePages();
   }
 
   ngOnChanges(changes: SimpleChanges): void {
+    this.calculatePages();
+  }
+
+  calculatePages() {
     if (this.totalItems) {
       this.totalPages = Math.ceil(this.totalItems / this.itemPerPage);
-      this.pages = Array.from({length: this.totalPages}, (_, i) => i + 1);
+      this.pages = Array.from({ length: this.totalPages }, (_, i) => i + 1);
+      this.updateDisplayedPages();
     }
   }
 
+  updateDisplayedPages() {
+    const visiblePages = 6;
+    const maxVisiblePages = Math.min(visiblePages, this.totalPages);
+    const startPages = this.pages.slice(0, 3);
+    const endPages = this.pages.slice(-3);
+    const middlePages: number[] = [];
+
+    if (this.totalPages <= visiblePages) {
+      this.displayedPages = this.pages;
+    } else {
+      if (this.currentPage <= 3) {
+        this.displayedPages = [...startPages, -1, ...endPages];
+      } else if (this.currentPage > this.totalPages - 3) {
+        this.displayedPages = [...startPages, -1, ...endPages];
+      } else {
+        middlePages.push(this.currentPage - 1, this.currentPage, this.currentPage + 1);
+        this.displayedPages = [...startPages, -1, ...middlePages, -1, ...endPages];
+      }
+    }
+  }
+
+  pageClicked(page: number | string) {
+    if (typeof page === 'number' && page > 0 && page <= this.totalPages) {
+      this.onClick.emit(page);
+      this.currentPage = page;
+      this.updateDisplayedPages();
+    }
+  }
 }
+
