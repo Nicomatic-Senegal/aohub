25ce4f02c6ecde5cd4512ebbdf058e3917659bee
kheuss1 <cheikhdiagne@esp.sn>
2025-04-18 01:13:38 +0200

    mvp stable git add .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: "http://208.85.21.177:8080/api/v1/"
-}
+  apiBaseUrl: 'https://aohub-back.nicomaticsenegal.com/api/v1/',
+  // apiBaseUrl: 'https://aohub-back.nicomaticsenegal.com/api/v1/',
+  //apiBaseUrl: "http://208.85.21.177:8080/api/v1/"
+};
